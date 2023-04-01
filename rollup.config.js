f92d5bda7f627193a7ab7325bd164bfb9eec7b0a
Ivo <example@example.com>
2023-04-01 23:25:40 +0200

    [Added] rollup

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default [
+    {
+        input: "src/main.ts",
+        output: {
+            dir: "dist",
+            format: "esm"
+        },
+        plugins: [
+            typescript()
+        ]
+    }
+]
