ba91bbe891d894d92404bf0b7e0c97a8cc91889e
Ivo <example@example.com>
2023-04-08 15:23:11 +0200

    [Updated] init function

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { container, registerDependencies, tokens } from "./utils";
 import type { ICredentialService } from "./services/credentialService";
 import type { IYouTubeService } from "./services/youtubeService";
 import type { IApplicationParameters } from "./services/applicationParameters";
+import mongoose from "mongoose";
 dotenv.config();
 
 async function main() {
     // Initialization
-    init();
+    try {
+        init();
+    } catch(e) {
+        throw new Error(e);
+    }
 
     const applicationParameters = container.get<IApplicationParameters>(tokens.ApplicationParams);
     const youtubeService = container.get<IYouTubeService>(tokens.YouTubeService);
@@ -16,9 +21,17 @@ async function main() {
     console.log(res);
 }
 
-function init() {
+// Run at start of app
+async function init(): Promise<void> {
     // Register dependencies
     registerDependencies();
+
+    // Connect to db
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    } catch {
+        throw new Error('Failed to connect to db');
+    }
 
     // Sets youtube api handler
     const credentialService = container.get<ICredentialService>(tokens.CredentialService);
