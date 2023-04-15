80dedcf619294dd278c80d12db51b5576b33d628
Ivo <example@example.com>
2023-04-15 23:03:23 +0200

    [Refactored] update video function

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { container, registerDependencies, tokens } from "./utils";
 import type { ICredentialService } from "./services/credentialService";
 import mongoose from "mongoose";
 import type { IYouTubeController } from "./controllers/youtubeController";
+
 dotenv.config();
 
 async function main() {
     // Initialization
     await init();
-
-    const youtubeController = container.get<IYouTubeController>(tokens.YouTubeController);
-    youtubeController.updateVideoList(true);
 
     process.exit(0);
 }
@@ -32,6 +30,11 @@ async function init(): Promise<void> {
     // Sets youtube api handler
     const credentialService = container.get<ICredentialService>(tokens.CredentialService);
     credentialService.init();
+}
+
+async function updateVideoList(updateAll: boolean) {
+    const youtubeController = container.get<IYouTubeController>(tokens.YouTubeController);
+    return youtubeController.updateVideoList(updateAll);
 }
 
 main();
