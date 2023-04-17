75886ed1dd1dad0656985e9904f1c7714811f6ce
Ivo <example@example.com>
2023-04-17 20:09:05 +0200

    [Added] interface

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,17 @@
 import type { youtube_v3 } from "googleapis";
 
 export type youtubePlaylistItemsParams = youtube_v3.Params$Resource$Playlistitems$List;
+
+// Information for that is expected to be acquired the HTML of the video page
+export interface IVideoCaptionInfo {
+    captionTracks: {
+        baseUrl: string;
+        name: {
+            simpleText: string
+        },
+        vssId: string,
+        languageCode: string,
+        kind: string,
+        isTranslatable:true
+    } []
+}
