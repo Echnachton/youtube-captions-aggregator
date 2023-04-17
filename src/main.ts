965b647c86a1679008b87c8b9c4628aed6caf022
Ivo <example@example.com>
2023-04-17 20:09:40 +0200

    [Updated] main function

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { container, registerDependencies, tokens } from "./utils";
 import type { ICredentialService } from "./services/credentialService";
 import mongoose from "mongoose";
 import type { IYouTubeController } from "./controllers/youtubeController";
+import { IYouTubeCaptionService } from "./services/youtubeCaptionService";
 
 dotenv.config();
 
@@ -11,7 +12,14 @@ async function main() {
     // Initialization
     await init();
 
-    await updateVideoList(false);
+    // await updateVideoList(false);
+
+    try {
+        const res = await getCaptions();
+        console.log(res);
+    } catch(e) {
+        console.error(e);
+    }
 
     console.log("Process exited successfully");
     process.exit(0);
@@ -38,6 +46,11 @@ async function init(): Promise<void> {
 async function updateVideoList(updateAll: boolean) {
     const youtubeController = container.get<IYouTubeController>(tokens.YouTubeController);
     return youtubeController.updateVideoList(updateAll);
+}
+
+async function getCaptions() {
+    const youtubeCaptionService = container.get<IYouTubeCaptionService>(tokens.YouTubeCaptionService);
+    return youtubeCaptionService.getCaptions("go", "fr");
 }
 
 main();
